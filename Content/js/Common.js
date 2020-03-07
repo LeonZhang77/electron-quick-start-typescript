a86f6817c588486a3ff60765854c41a6acb429f0
Leon Zhang <zhangjianfeng73@hotmail.com>
2020-03-07 19:58:26 +0800

    add assert and content

---
+++ Content/js/Common.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    $("#ApplicationType").change(function () {
+        var applicationId = $("#ApplicationType option:selected").text();
+        var droneId = $("#DroneId");
+        var VersionId = $("#VersionId");
+
+        $.ajax({
+            url: "/DroneConfig/GetApplicationVersionCodeJsonResult/?applicationName=" + applicationId + "&droneId=" + droneId,
+            type: "get",
+            dataType: "json",
+            contentType: "application/json",
+            success: function (result) {
+                var options = "";
+                VersionId.html("");
+                $.each(result, function (i, data) {
+                    options += "<option value=" + data.VersionId + ">" + data.VersionCode + "</option>";
+                });
+                VersionId.append(options);
+            }
+        });
+    });
+});
